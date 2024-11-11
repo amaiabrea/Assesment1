7bafbe4afd2e9bd006f2b004dbb43d6f0af95f08
amaiabrea <amaiabrea@gmail.com>
2024-11-11 12:20:39 +1000

    Highest & lowest marks
    Calculate and display highest and lowest marks

--- StudentsMark.java
+++ StudentsMark.java
@@ -55,7 +55,6 @@ public class StudentsMark
             System.out.println(mark);        
         }
         //Calculate and display highest and lowest mark
-        
         float highest=marks[0];
         float lowest =marks[0];
         
