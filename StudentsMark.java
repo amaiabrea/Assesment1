524d39eef18074ec045ad5c04e22042432586cdd
amaiabrea <amaiabrea@gmail.com>
2024-11-11 12:17:31 +1000

    Changed line
    Changed the number of students from 5 to 30 students

--- StudentsMark.java
+++ StudentsMark.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class StudentsMark
 {
     public static void main(String [] args){
-        int numberOfStudents=5;
+        int numberOfStudents=30;
         
         float[] marks= new float[numberOfStudents];
         
